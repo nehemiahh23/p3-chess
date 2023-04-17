d1d6d6c6faa43165971158b2d91abeb0602b2987
Nehemiah Henry <nehemiahelihenry@gmail.com>
2023-04-16 21:19:00 -0400

    rename attrs and add repr

--- lib/classes/pawn.py
+++ lib/classes/pawn.py
@@ -2,6 +2,9 @@ class Pawn:
 
     def __init__(self, pos):
         self.pos = pos
-        self.move_forward = [pos[0] + 1, pos[1]]
-        self.move_diag_r = [pos[0] + 1, pos[1] + 1]
-        self.move_diag_l = [pos[0] + 1, pos[1] - 1]
+        self.forward_pos = [pos[0] - 1, pos[1]] # it goes to -1, prevent this
+        self.diag_l_pos = [pos[0] - 1, pos[1] - 1]
+        self.diag_r_pos = [pos[0] - 1, pos[1] + 1]
+
+    def __repr__(self):
+        return "♟"
