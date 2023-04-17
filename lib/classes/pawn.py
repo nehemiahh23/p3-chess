1bdf21359b68b03be35a755c14fc7c79ef0f5f29
Nehemiah Henry <nehemiahelihenry@gmail.com>
2023-04-17 15:46:11 -0400

    initial and team based pawn movement

--- lib/classes/pawn.py
+++ lib/classes/pawn.py
@@ -2,13 +2,23 @@ class Pawn:
 
     def __init__(self, pos, black=False):
         self.pos = pos
+        self._start = pos
         self.black = black
         self.calc()
 
     def calc(self):
-        self.n_pos = [self.pos[0] - 1, self.pos[1]]
-        self.nw_pos = [self.pos[0] - 1, self.pos[1] - 1]
-        self.ne_pos = [self.pos[0] - 1, self.pos[1] + 1]
+        if self.black == False:
+            if self.pos == self._start:
+                self.n2_pos = [self.pos[0] - 2, self.pos[1]]
+            self.n_pos = [self.pos[0] - 1, self.pos[1]]
+            self.nw_pos = [self.pos[0] - 1, self.pos[1] - 1]
+            self.ne_pos = [self.pos[0] - 1, self.pos[1] + 1]
+        else:
+            if self.pos == self._start:
+                self.s2_pos = [self.pos[0] + 2, self.pos[1]]
+            self.s_pos = [self.pos[0] + 1, self.pos[1]]
+            self.sw_pos = [self.pos[0] + 1, self.pos[1] - 1]
+            self.se_pos = [self.pos[0] + 1, self.pos[1] + 1]
 
     def __repr__(self):
         if self.black:
