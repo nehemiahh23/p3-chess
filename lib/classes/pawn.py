ecde7ea3032308a7649235b5de7560ca447536a4
Nehemiah Henry <nehemiahelihenry@gmail.com>
2023-04-17 10:46:59 -0400

    rename pawn moves

--- lib/classes/pawn.py
+++ lib/classes/pawn.py
@@ -2,9 +2,9 @@ class Pawn:
 
     def __init__(self, pos):
         self.pos = pos
-        self.forward_pos = [pos[0] - 1, pos[1]] # it goes to -1, prevent this
-        self.diag_l_pos = [pos[0] - 1, pos[1] - 1]
-        self.diag_r_pos = [pos[0] - 1, pos[1] + 1]
+        self.n_pos = [pos[0] - 1, pos[1]] # it goes to -1, prevent this
+        self.nw_pos = [pos[0] - 1, pos[1] - 1]
+        self.ne_pos = [pos[0] - 1, pos[1] + 1]
 
     def __repr__(self):
         return "♟"
